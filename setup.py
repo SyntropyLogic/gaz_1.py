8d8da3154dd9633cc2d7676927ccf11f57b67e4e
Synergy Logic <196492677+SyntropyLogic@users.noreply.github.com>
2025-08-29 16:44:34 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 from glob import glob
 
@@ -7,11 +7,11 @@ package_name = 'usl_drone_project'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=['src'], # Tell setuptools our python code is in the 'src' folder
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/''/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
@@ -23,12 +23,12 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'usl_main = src.main:main',
-            'usl_command_gate = src.command_gate:main',
-            'usl_adaptive_controller = src.adaptive_controller:main',
-            'usl_intent_translator = src.intent_translator:main',
-            'usl_mission = src.entropic_mission_ena:main',
-            'usl_simple_mover = src.simple_mover:main',
+            'usl_main = usl_drone_project.main:main',
+            'usl_command_gate = usl_drone_project.command_gate:main',
+            'usl_adaptive_controller = usl_drone_project.adaptive_controller:main',
+            'usl_intent_translator = usl_drone_project.intent_translator:main',
+            'usl_mission = usl_drone_project.entropic_mission_ena:main',
+            'usl_simple_mover = usl_drone_project.simple_mover:main',
         ],
     },
 )
