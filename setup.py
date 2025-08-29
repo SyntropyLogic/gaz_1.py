205e72e34df6a1ded4a731a43dbe0cf7fe00bec9
Synergy Logic <196492677+SyntropyLogic@users.noreply.github.com>
2025-08-29 19:14:07 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup, find_packages
-import os
-from glob import glob
+from setuptools import setup
 
 package_name = 'usl_drone_project'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[
+        # Explicitly tell setup where to find the python code
+        'usl_drone_project.usl_drone_project' 
+    ],
+    # Tell setup where the root of our python package is
+    package_dir={'': '.'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/''/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/usl_gazebo.launch.py'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,12 +26,13 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'usl_main = usl_drone_project.main:main',
-            'usl_command_gate = usl_drone_project.command_gate:main',
-            'usl_adaptive_controller = usl_drone_project.adaptive_controller:main',
-            'usl_intent_translator = usl_drone_project.intent_translator:main',
-            'usl_mission = usl_drone_project.entropic_mission_ena:main',
-            'usl_simple_mover = usl_drone_project.simple_mover:main',
+            # The format is 'executable_name = package.module:main'
+            'usl_main = usl_drone_project.usl_drone_project.main:main',
+            'usl_command_gate = usl_drone_project.usl_drone_project.command_gate:main',
+            'usl_adaptive_controller = usl_drone_project.usl_drone_project.adaptive_controller:main',
+            'usl_intent_translator = usl_drone_project.usl_drone_project.intent_translator:main',
+            'usl_mission = usl_drone_project.usl_drone_project.entropic_mission_ena:main',
+            'usl_simple_mover = usl_drone_project.usl_drone_project.simple_mover:main',
         ],
     },
 )
