fd9a6f11d562d0c437fecfbd4accf9900f705bae
Synergy Logic <196492677+SyntropyLogic@users.noreply.github.com>
2025-08-29 19:34:52 +1000

    Create setup_2.py

---
+++ setup_2.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'usl_drone_project'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    # find_packages() will automatically discover our 'usl_drone_project' python module
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Crispy',
+    maintainer_email='user@todo.todo',
+    description='Unified Synergy Logic (USL) Drone Controller',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # The format is now 'executable_name = python_module_name.file_name:main'
+            'usl_main = usl_drone_project.main:main',
+            'usl_command_gate = usl_drone_project.command_gate:main',
+            'usl_adaptive_controller = usl_drone_project.adaptive_controller:main',
+            'usl_intent_translator = usl_drone_project.intent_translator:main',
+            'usl_mission = usl_drone_project.entropic_mission_ena:main',
+            'usl_simple_mover = usl_drone_project.simple_mover:main',
+        ],
+    },
+)
